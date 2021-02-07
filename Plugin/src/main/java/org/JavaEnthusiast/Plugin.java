0d3427cdf4a5fc07edea14a0c44ef4eee9f1ba5d
maini <philip.mattsson@iths.se>
2021-02-07 21:25:44 +0100

    Version 0.2

---
+++ Plugin/src/main/java/org/JavaEnthusiast/Plugin.java
@@ -0,0 +1,10 @@
+package org.JavaEnthusiast;
+
+import java.util.ServiceLoader;
+
+public class Plugin {
+
+    public static void main(String[] args) {
+
+    }
+}
--- Server/src/main/java/org/JavaEnthusiast/Server.java
+++ Server/src/main/java/org/JavaEnthusiast/Server.java
@@ -1,34 +1,96 @@
 package org.JavaEnthusiast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //https://dzone.com/articles/simple-http-server-in-java
-        //https://netty.io/
-        int portNumber = 4004;
 
-        try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out =
-                        new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            String inputLine, outputLine;
-            while ((inputLine = in.readLine()) != null) {
-                out.println("Hello World");
+        File file = new File("web\\index.html");
+        new Server().readFromFile(file);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(5050);
+            System.out.println(Thread.currentThread());
+
+            while (true){
+                Socket socket = serverSocket.accept();
+
+                executorService.execute(() -> handleConnections(socket));
             }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleConnections(Socket socket) {
+        System.out.println(Thread.currentThread());
+
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            while (true){
+                String headerLine = input.readLine();
+                System.out.println(headerLine);
+                if (headerLine.isEmpty())
+                    break;
+            }
+
+            var output = new PrintWriter(socket.getOutputStream());
+            String page = """
+                    <html>
+                    <head>
+                    <title>Hello World!</title>
+                    </head>
+                    <body>
+                    <h1>Hello There!</h1>
+                    <div>First Page</div>
+                    </body>
+                    </html>""";
+
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Length:" + page.getBytes().length);
+            output.println("Content-Type:text/html");
+            output.println("");
+            output.println(page);
+
+            output.flush();
+            socket.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+//    private static void Json(){
+//
+//
+//        JsonConverter converter = new JsonConverter();
+//
+//        var json = converter.ConvertToJson();
+//        System.out.println(json);
+//    }
+
+    private byte[] readFromFile(File file){
+        byte[] content = new byte[0];
+        System.out.println("Does file exists: " + file.exists());
+        if(file.exists() && file.canRead()){
+            try (FileInputStream fileInputStream = new FileInputStream(file)){
+                content = new byte[(int) file.length()];
+                int count = fileInputStream.read(content);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return content;
     }
 }
