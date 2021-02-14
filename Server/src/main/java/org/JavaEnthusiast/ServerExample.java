7c2e861448109dcd40c86e179b72bd3a44e20e31
maini <philip.mattsson@iths.se>
2021-02-14 14:23:10 +0100

    Version 1.4

--- Server/src/main/java/org/JavaEnthusiast/ServerExample.java
+++ Server/src/main/java/org/JavaEnthusiast/ServerExample.java
@@ -5,7 +5,6 @@ import org.JavaEnthusiast.FileUtils.FileReader;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
